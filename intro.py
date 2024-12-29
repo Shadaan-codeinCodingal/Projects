191fa5a3f413b7516660aab29d5b4765c8e9bbda
Shadaan-codeinCodingal <taniz.4532@gmail.com>
2024-12-29 11:53:20 +0530

    Intro

---
+++ intro.py
@@ -0,0 +1,7 @@
+print("Welcome to Codingal!", end=" ")
+print("Hope you are doing great!")
+print("Coding is very fun.")
+print("My name is Shadaan.\nI am 9 years old.")
+print("Hello", "Cody", sep=" ##")
+print("I am 9 years old.")
+# This is an introdution to Python.
