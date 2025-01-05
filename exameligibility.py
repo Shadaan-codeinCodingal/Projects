8387e1dc3f59f548b01395d16e7e877fb7a57718
Shadaan-codeinCodingal <taniz.4532@gmail.com>
2025-01-05 16:04:34 +0530

    Add files via upload

---
+++ exameligibility.py
@@ -0,0 +1,11 @@
+state = input("Are you ill or not ill?(Write in small caps as 'ill' or 'not ill')")
+present = int(input("What percentage of the year have you been present?(Write number only)")) 
+if state == "not ill":
+    if present >= 75:
+        print("You may enter the exam")
+    else:
+        print("You cannot enter the exam.")
+elif state == "ill":
+    print("You cannot enter the exam")
+else:
+    print("Error!")
