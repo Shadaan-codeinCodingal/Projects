a6075cd72b4ac5af8a062407f8bbbd2e01bb0650
Shadaan-codeinCodingal <taniz.4532@gmail.com>
2025-01-05 10:43:45 +0530

    Add files via upload

---
+++ numberswap.py
@@ -0,0 +1,7 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+print("Number 1 and Number 2 are",a,b)
+c = a
+a = b
+b = c
+print("Number 1 and Number 2 are",a,b)
