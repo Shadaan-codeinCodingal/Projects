71d4e56723fce292a1b922295385a463071dee48
Shadaan-codeinCodingal <taniz.4532@gmail.com>
2025-01-18 12:18:49 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,14 @@
+dictionary = {'Name':'Shadaan', 9: 'Age'}
+print(dictionary)
+dictionary = {'Name':'Shadaan', 'Sports':['Tennis', 'Cricket']}
+print(dictionary)
+dictionary = {'Name': 'Zinat', 'Age': 33}
+print(dictionary)
+print(dictionary['Age'])
+print(dictionary.get('Name'))
+dictionary['City'] = 'Hyderabad'
+print(dictionary)
+dictionary.pop('Age')
+print(dictionary)
+dictionary.clear()
+print(dictionary)
