be87888f41e6291d6c759e9239eb2ceeca0db2a3
Shadaan-codeinCodingal <taniz.4532@gmail.com>
2025-01-19 18:25:07 +0530

    Add files via upload

---
+++ tuple-list.py
@@ -0,0 +1,6 @@
+information = ("Shadaan", "Ali Khan", 9, "141 cm", "26 kg","Math")
+print("Original Tuple", information)
+list = []
+for i in range(len(information)):
+  list.append(information[i])
+print("Tuple Converted into list:", list)
