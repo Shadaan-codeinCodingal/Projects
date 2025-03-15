f628aa4ca6ff005d50a93488f6612a92fdc0119f
Shadaan-codeinCodingal <taniz.4532@gmail.com>
2025-03-15 11:34:30 +0530

    Update Analysis.py

---
+++ Analysis.py
@@ -0,0 +1,18 @@
+# for function 1
+# It will always take 1 iteration as 
+# a) It did not take a loop
+# b) it took an arithmetic calculation thus giving a result of 1 iteration
+# Also, this means it took the lowest time and iterations.
+
+# For function 2
+# It will take n iterations as
+# a) it was run in a loop
+# b) there were n numbers in the calculation which was added n times thus giving a result of n iterations
+# It took an average time but will take a very long time to calculate if n was a large number
+
+# For function 3
+# It will take the sum from 1 to n iterations as
+# a) it had an inner loop and an outer loop
+# b) the inner loop decomposed the value of j into groups of 1 adding j iterations
+# c) this means it decomposed all values of j into groups of 1 making it the sum of 1 to n iterations
+# It has taken the longest time and even could take more space especially if n is a large number.
